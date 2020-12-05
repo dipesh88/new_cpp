7a61cb9832945e311312db053d41fa450c819fcc
dipesh88 <63647097+dipesh88@users.noreply.github.com>
2020-12-05 18:27:02 +0530

    new

--- even.cpp
+++ even.cpp
@@ -1,13 +1,25 @@
-//
-#include<iostream>
-#include<conio.h>
+// print even number
 
-void main()
+#include <iostream>
+#include <conio.h>
+using namespace std;
+void even_num(int n)
+{
+    int i = 0;
+    for (i = 0; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            cout << i << endl;
+        }
+    }
+}
 
 int main()
 {
-
+    int i = 0;
+    cout << "firse number de" << endl;
+    cin >> i;
+    even_num(i);
+    return 0;
 }
-
-
-
